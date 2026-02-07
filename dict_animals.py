f4e77491ad6281e6353ec2dda79c9bdf28a2155d
alekseewatatianawork73 <153320481+alekseewatatianawork73@users.noreply.github.com>
2026-02-07 11:05:52 +0300

    Update dict_animals.py

--- dict_animals.py
+++ dict_animals.py
@@ -6,8 +6,12 @@ articles = ['Foxes are small-to-medium-sized omnivorous mammals belonging to sev
             'Hares and jackrabbits are mammals belonging to the genus Lepus...',
             'The wolf, also known as the grey wolf or gray wolf, is a canine native to Eurasia and North America...',
             'Bears are carnivoran mammals of the family Ursidae. They are classified as...']
-# выведем статью о волке
-print(animals[2], articles[2], sep=' - ')  # => wolf - The wolf, also known as the grey wolf or gray wolf, is a canine native to Eurasia and North America...
+# найдём и выведем статью о волке
+n = len(animals)  # количество статей
+for i in range(n):
+    if animals[i] == 'wolf':
+        print(animals[i], articles[i], sep=' - ')
+# => wolf - The wolf, also known as the grey wolf or gray wolf, is a canine native to Eurasia and North America...
 
 # 2 способ: список кортежей, каждый кортеж содержит два элемента - название животного и статью о нём
 animals = [('fox', 'Foxes are small-to-medium-sized omnivorous mammals belonging to several genera...'), ('hare', 'Hares and jackrabbits are mammals belonging to the genus Lepus...'), 
@@ -20,7 +24,20 @@ for kortezh in animals:
 # =>  Bears are carnivoran mammals of the family Ursidae. They are classified as...
 
 # 3 способ - самый удобный: словарь, в котором значениями являются статьи о животных, а ключом для каждой статьи - название животного
-animals = {'fox': 'Foxes are small-to-medium-sized omnivorous mammals belonging to several genera...', 
-           'hare': 'Hares and jackrabbits are mammals belonging to the genus Lepus...', 
-           'wolf': 'The wolf, also known as the grey wolf or gray wolf, is a canine native to Eurasia and North America...', 
-           'bear': 'Bears are carnivoran mammals of the family Ursidae. They are classified as...'}
+animals = {'fox': 'Foxes are small-to-medium-sized omnivorous mammals belonging to several genera...',
+           'hare': 'Hares and jackrabbits are mammals belonging to the genus Lepus...',
+           'wolf': 'The wolf, also known as the grey wolf or gray wolf, is a canine native to Eurasia and North America...'}
+# найдём и выведем статью о лисе
+print(animals['fox'])
+# => Foxes are small-to-medium-sized omnivorous mammals belonging to several genera...
+
+# изменение значения в словаре: заменим статью о зайце (hare) на новую
+animals['hare'] = 'New article about hare...'
+print(animals['hare'])  # => New article about hare...
+
+# добавление нового элемента в словарь: добавим статью о медведе
+animals['bear'] = 'Bears are carnivoran mammals of the family Ursidae. They are classified as...'
+
+# удаление элементов из словаря
+del animals['hare']
+print(animals['hare'])  # => ошибка: KeyError: 'hare' - такого ключа больше нет в словаре
