5f4a96b7863016c2af6ceb4bde4d839a04db79cd
alekseewatatianawork73 <153320481+alekseewatatianawork73@users.noreply.github.com>
2026-02-07 09:00:37 +0300

    Create dict_animals.py

---
+++ dict_animals.py
@@ -0,0 +1,5 @@
+animals = ['fox', 'hare', 'wolf', 'bear']
+articles = ['Foxes are small-to-medium-sized omnivorous mammals belonging to several genera...',
+            'Hares and jackrabbits are mammals belonging to the genus Lepus...',
+            'The wolf, also known as the grey wolf or gray wolf, is a canine native to Eurasia and North America...',
+            'Bears are carnivoran mammals of the family Ursidae. They are classified as...']
